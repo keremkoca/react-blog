110270a1b2fedd94be13846236edf2a16482ca9c
keremkoca <kerem-koca@yandex.com>
2022-01-25 16:38:56 +0300

    added login and register validation with formik

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,23 +1,106 @@
 import classes from "./Login.module.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 function Login() {
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.login}>
-        <form>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email"></input>
-          <label htmlFor="password">Password</label>
-          <input name="password" type="password"></input>
-          <button className={classes.button} type="submit">
-            LOGIN
-          </button>
-        </form>
-        <Link to="/register">Dont have account?Sign up</Link>
-      </div>
-    </div>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={(values) => {
+        console.log(values);
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        const passwordRegex = /(?=.*[0-7])/;
+        if (!values.password) {
+          errors.password = "Required";
+        } else if (values.password.length < 8) {
+          errors.password = "Password must be 6 characters long.";
+        } else if (!passwordRegex.test(values.password)) {
+          errors.password = "Invalid password. Must contain one number.";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        setTimeout(() => {
+          console.log("Logging in", values);
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        } = props;
+
+        return (
+          <div className={classes.wrapper}>
+            <div className={classes.login}>
+              <Form className={classes.form}>
+                <label className={classes.label} htmlFor="email">
+                  Email
+                </label>
+                <Field
+                  className={classes.field}
+                  id="email"
+                  name="email"
+                  type="text"
+                  placeholder="Enter your email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <ErrorMessage
+                  className={classes.error}
+                  name="email"
+                  component="div"
+                />
+                <label className={classes.label} htmlFor="password">
+                  Password
+                </label>
+                <Field
+                  className={classes.field}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <ErrorMessage
+                  className={classes.error}
+                  name="password"
+                  component="div"
+                />
+                <button
+                  className={classes.button}
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Submit
+                </button>
+              </Form>
+              <Link to="/register">Dont have account?Sign up</Link>
+            </div>
+          </div>
+        );
+      }}
+    </Formik>
   );
 }
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,28 +1,148 @@
 import classes from "./Register.module.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 
 function Register() {
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.register}>
-        <h2>Create Account</h2>
-        <form>
-          <label>Username</label>
-          <input name="username" type="text"></input>
-          <label>Email</label>
-          <input name="email" type="email"></input>
-          <label>Password</label>
-          <input name="password" type="password"></input>
-          <label>Confirm Password</label>
-          <input name="confirmPassword" type="password"></input>
-          <button className={classes.button} type="submit">
-            Create Account
-          </button>
-        </form>
-        <Link to="/login">Already have an account?Sign in</Link>
-      </div>
-    </div>
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validate={(values) => {
+        console.log(values);
+        const errors = {};
+        if (!values.username) {
+          errors.username = "Required";
+        }
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        const passwordRegex = /(?=.*[0-7])/;
+        if (!values.password) {
+          errors.password = "Required";
+        } else if (values.password.length < 8) {
+          errors.password = "Password must be 6 characters long.";
+        } else if (!passwordRegex.test(values.password)) {
+          errors.password = "Invalid password. Must contain one number.";
+        }
+        const confirmPasswordRegex = /(?=.*[0-7])/;
+        if (!values.confirmPassword) {
+          errors.confirmPassword = "Required";
+        } else if (values.confirmPassword.length < 8) {
+          errors.confirmPassword = "confirmPassword must be 6 characters long.";
+        } else if (!confirmPasswordRegex.test(values.confirmPassword)) {
+          errors.confirmPassword =
+            "Invalid confirmPassword. Must contain one number.";
+        } else if (values.password !== values.confirmPassword) {
+          errors.confirmPassword = "Passwords do not match";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        setTimeout(() => {
+          console.log("Logging in", values);
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        } = props;
+        return (
+          <div className={classes.wrapper}>
+            <div className={classes.register}>
+              <h2 className={classes.h2}>Create Account</h2>
+              <Form className={classes.form}>
+                <label className={classes.label}>Username</label>
+                <Field
+                  className={classes.field}
+                  name="username"
+                  type="text"
+                  id="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                ></Field>
+                <ErrorMessage
+                  className={classes.error}
+                  name="username"
+                  component="div"
+                />
+                <label className={classes.label}>Email</label>
+                <Field
+                  className={classes.field}
+                  name="email"
+                  type="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                ></Field>
+                <ErrorMessage
+                  className={classes.error}
+                  name="email"
+                  component="div"
+                />
+                <label className={classes.label}>Password</label>
+                <Field
+                  className={classes.field}
+                  name="password"
+                  type="password"
+                  id="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                ></Field>
+                <ErrorMessage
+                  className={classes.error}
+                  name="password"
+                  component="div"
+                />
+                <label className={classes.label}>Confirm Password</label>
+                <Field
+                  className={classes.field}
+                  name="confirmPassword"
+                  type="password"
+                  id="confirmPassword"
+                  value={values.confirmPassword}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                ></Field>
+                <ErrorMessage
+                  className={classes.error}
+                  name="confirmPassword"
+                  component="div"
+                />
+                <button
+                  className={classes.button}
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Create Account
+                </button>
+              </Form>
+              <Link to="/login">Already have an account?Sign in</Link>
+            </div>
+          </div>
+        );
+      }}
+    </Formik>
   );
 }
 
